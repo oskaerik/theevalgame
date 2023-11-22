07ce5b42420382b1c4ce15b844fdc338e58a761f
Oskar Eriksson <oskaerik@gmail.com>
2023-11-22 20:40:32 +0100

    Add evaluate_objectives

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,15 @@
 """Core library."""
 
 
+def evaluate_objectives(code: str, objectives: dict) -> dict:
+    """Evaluate objectives."""
+    symbols = run(code)
+    return {
+        var: (val == symbols[var]) if var in symbols else False
+        for var, val in objectives.items()
+    }
+
+
 def run(code: str) -> dict:
     """Run code."""
     symbols = {}
--- tests/test_all.py
+++ tests/test_all.py
@@ -1,8 +1,8 @@
 """Tests."""
-from src import run
+from src import evaluate_objectives, run
 
 
-def test_run() -> None:
+def test_run_returns_symbols() -> None:
     """Test."""
     # Arrange
     code = """
@@ -16,3 +16,19 @@ s = a + b
 
     # Assert
     assert symbols == {"a": 7, "b": 3, "s": 10}
+
+
+def test_evaluate_objectives_returns_correct() -> None:
+    """Test."""
+    # Arrange
+    code = """
+a = 7
+b = 3
+"""
+    objectives = {"a": 7, "b": 3, "s": 10}
+
+    # Act
+    result = evaluate_objectives(code, objectives)
+
+    # Assert
+    assert result == {"a": True, "b": True, "s": False}
