4f6f6267d6d20df487c66cd55f6609f81fa46102
Oskar Eriksson <oskaerik@gmail.com>
2023-11-22 21:06:49 +0100

    Allow initial symbols

--- src/__init__.py
+++ src/__init__.py
@@ -1,17 +1,17 @@
 """Core library."""
 
 
+def run(code: str, symbols: dict | None = None) -> dict:
+    """Run code."""
+    symbols = symbols or {}
+    exec(code, symbols)  # noqa: S102
+    del symbols["__builtins__"]
+    return symbols
+
+
 def evaluate_objectives(symbols: dict, objectives: dict) -> dict:
     """Evaluate objectives."""
     return {
         var: (val == symbols[var]) if var in symbols else False
         for var, val in objectives.items()
     }
-
-
-def run(code: str) -> dict:
-    """Run code."""
-    symbols = {}
-    exec(code, symbols)  # noqa: S102
-    del symbols["__builtins__"]
-    return symbols
--- tests/test_all.py
+++ tests/test_all.py
@@ -2,8 +2,7 @@
 from src import evaluate_objectives, run
 
 
-def test_run_returns_symbols() -> None:
-    """Test."""
+def test_run_without_initial_symbols_returns_symbols() -> None:
     # Arrange
     code = """
 a = 7
@@ -18,8 +17,21 @@ s = a + b
     assert symbols == {"a": 7, "b": 3, "s": 10}
 
 
-def test_evaluate_objectives_returns_correct() -> None:
-    """Test."""
+def test_run_with_initial_symbols_returns_symbols() -> None:
+    # Arrange
+    code = """
+b = 3
+s = a + b
+"""
+
+    # Act
+    symbols = run(code, symbols={"a": 7})
+
+    # Assert
+    assert symbols == {"a": 7, "b": 3, "s": 10}
+
+
+def test_evaluate_objectives_returns_result() -> None:
     # Arrange
     symbols = {"a": 7, "b": 3}
     objectives = {"a": 7, "b": 3, "s": 10}
